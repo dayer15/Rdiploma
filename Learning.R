0968ef7003851c960f49b22b3e2fff8f5f3326f7
dayer15 <davorsestan@gmail.com>
2021-02-06 23:53:30 +0000

    06fEB21

--- Learning.R
+++ Learning.R
@@ -1,7 +1,8 @@
 # Write output to a file
 write.csv(sumData, file = "000sum.csv", row.names = FALSE)
 read.csv("000sum.csv")
-
+install.packages("xml2")
+library("xml2")
 # Install and load necessary packages
 install.packages("rvest")
 library(rvest)
@@ -13,7 +14,15 @@ SFGTables <- dataSFG %>% html_table(fill = TRUE)
 teamBatting <- SFGTables[[1]]
 head(teamBatting)
 # Read "Team Pitching" table
-teamPitching <- allTables[[2]]
+teamPitching <- SFGTables[[2]]
 head(teamPitching)
-
-
+#Davor filtering
+teamBatting<-subset(teamBatting, teamBatting$Rk!="Rk")
+teamBatting<-subset(teamBatting, teamBatting$Rk!="")
+tail(teamBatting)
+rownames(teamBatting)<-NULL
+teamBatting$Name<-gsub("\\*","",teamBatting$Name)
+teamBatting$Name<-gsub("\\#","",teamBatting$Name)
+teamBatting
+#filtering data finished
+write.csv(teamBatting, "teamBatting.csv", row.names = F)
